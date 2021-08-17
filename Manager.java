063282c8d5ad114825265b1636e8429663514746
PankajSre <pankaj.saini85@gmail.com>
2021-08-17 14:42:46 +0530

    Vehicle.java was added

--- Manager.java
+++ Manager.java
@@ -2,5 +2,6 @@ public class Manager {
    public static void main(String[] args){
       System.out.println("Hello from Manager");
 	  System.out.println("Hello from Manager Again");
+	  System.out.println("Git Repository");
    }
 }
