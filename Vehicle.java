b751082359cc6f4e552fed89af8f2cb215cdbeee
PankajSre <pankaj.saini85@gmail.com>
2021-08-17 14:55:20 +0530

    Vehicle.java modified

--- Vehicle.java
+++ Vehicle.java
@@ -1,5 +1,6 @@
 public class Vehicle {
    public static void main(String[] args){
       System.out.println("Hello from Vehicle class"); 
+	   System.out.println("Hello from Git repository"); 
    }
 }
