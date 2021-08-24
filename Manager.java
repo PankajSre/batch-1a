f198960fff300aefc15af2417ea24b93dd0e714f
PankajSre <pankaj.saini85@gmail.com>
2021-08-24 14:36:53 +0530

    Manager class final change

--- Manager.java
+++ Manager.java
@@ -4,7 +4,7 @@ public class Manager {
 	  System.out.println("Hello from Manager Again");
 	  System.out.println("Git Repository");
 	  System.out.println("For Jenkins");
-	  System.out.println("With the help of WebHook");
+	  System.out.println("With the help of WebHook------");
 	  System.out.println("For auto triggering the changes");
 	  
    }
